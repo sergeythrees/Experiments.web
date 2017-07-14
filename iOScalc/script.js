02391e3facd41795e8198b4ddf1e89c02ecf03a3
sergeythrees <sergeythrees@gmail.com>
2017-07-14 13:12:14 +0300

    реализовал основную логику калькулятора

--- iOScalc/script.js
+++ iOScalc/script.js
@@ -1,10 +1,106 @@
+//global
+var
+  DISPLAY_ID = 'displayId',
+  display = document.getElementById(DISPLAY_ID),
+  lastSavedDigit = 0,
+  secondDigit = null,
+  currentOperation = null,
+  clearDisplayFlag = false;
+
+//main
 for (var i=0; i<=9; ++i) {
-  makeButtonClickHandler(i);
+  makeDigitButtonClickHandler(i);
 }
 
-function makeButtonClickHandler(buttonId) {
-  var button = document.getElementById(buttonId),
-      display = document.getElementById('displayId');
-  button.addEventListener("click",
-  function (){display.firstChild.data = button.firstChild.data;});
+[ ["AC", clear],
+  ["+/-", changeSign],
+  ["%", percent],
+  ["÷", function(){changeCurrentOperation(divide)}],
+  ["*", function(){changeCurrentOperation(multiple)}],
+  ["-", function(){changeCurrentOperation(substract)}],
+  ["+", function(){changeCurrentOperation(sum)}],
+  ["=", currentOperation],
+  [",", changeSign],
+ ]
+.forEach(function(item) {
+  var
+    button = document.getElementById(item[0]);
+    button.onclick = item[1];
+});
+
+//functions
+function changeCurrentOperation(operation) {
+  currentOperation = operation;
+  secondDigit = null;
+  lastSavedDigit = display.firstChild.data;
+  clearDisplayFlag = false;
+  document.getElementById("=").onclick = function(){
+    if (secondDigit == null)
+      secondDigit = display.firstChild.data;
+    currentOperation();};
+}
+
+function makeDigitButtonClickHandler(buttonId) {
+  var
+    button = document.getElementById(buttonId);
+  button.onclick =  function (){
+    displayPressedDigit(button.firstChild.data)};
+}
+
+function displayPressedDigit(digit) {
+  if (display.firstChild.data == 0) {
+    lastSavedDigit = display.firstChild.data;
+    display.firstChild.data = digit;
+  }
+  else {
+    if (currentOperation != null && clearDisplayFlag == false) {
+      lastSavedDigit = display.firstChild.data;
+      display.firstChild.data = digit;
+      clearDisplayFlag = true;
+    }
+    else {
+        display.firstChild.data = display.firstChild.data + digit;
+    }
+  }
+}
+
+function displayOperationResult(digit) {
+  display.firstChild.data = digit;
+  lastSavedDigit = digit;
+}
+
+function clear() {
+  lastSavedDigit = 0;
+  secondDigit = null;
+  display.firstChild.data = 0;
+  changeCurrentOperation(null);
+}
+
+function changeSign() {
+  display.firstChild.data = -display.firstChild.data;
+}
+
+function percent() {
+  display.firstChild.data = +lastSavedDigit / 100 * +display.firstChild.data;
+  lastSavedDigit = display.firstChild.data;
+}
+
+function divide() {
+  display.firstChild.data = +lastSavedDigit / +secondDigit;
+  lastSavedDigit = display.firstChild.data;
+}
+
+function multiple() {
+  display.firstChild.data = lastSavedDigit * secondDigit;
+  lastSavedDigit = display.firstChild.data;
+}
+
+function substract() {
+  display.firstChild.data = lastSavedDigit - secondDigit;
+  lastSavedDigit = display.firstChild.data;
+}
+
+function sum() {
+  display.firstChild.data = +lastSavedDigit + +secondDigit;
+  lastSavedDigit = display.firstChild.data;
 }
